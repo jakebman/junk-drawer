3891e1713fc2b3b0114fee92b4a0d2b26c3ac1f3
Jake Boeckerman <jakebman@gmail.com>
2024-06-10 10:50:45 -0600

    refactor: split a long command into two; doc it

--- update-junk-drawer.sh
+++ update-junk-drawer.sh
@@ -2,4 +2,8 @@
 
 THIS_SCRIPT="$(realpath "${BASH_SOURCE[0]:-$0}")"
 cd "$(dirname "$THIS_SCRIPT")"
-UPDATE_JUNK_DRAWER=any-string-value mr up
+
+# The purpose of this script is to provide this flag to mr, run in the same directory as this script
+# We've cd'd to the right directory, so now we set the flag, and then we run mr
+export UPDATE_JUNK_DRAWER=any-string-value
+mr up
