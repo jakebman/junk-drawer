9f25ce1559c769945ccf28698401bac0f3d76eb1
Jake Boeckerman <jakebman@gmail.com>
2024-10-30 15:09:59 -0600

    migrated this functionality to my bash pull function

--- update-junk-drawer.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-THIS_SCRIPT="$(realpath "${BASH_SOURCE[0]:-$0}")"
-cd "$(dirname "$THIS_SCRIPT")"
-
-# The purpose of this script is to provide this flag to mr, run in the same directory as this script
-# We've cd'd to the right directory, so now we set the flag, and then we run mr
-export UPDATE_JUNK_DRAWER=any-string-value
-mr up
